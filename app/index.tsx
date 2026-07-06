2ec8b56819ce2522859ccad4755b2823e62d41e2
aurielfritz27 <176544501+aurielfritz27@users.noreply.github.com>
2026-07-06 14:25:36 +0800

    add 3rd number

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Lloyd N. Tito, 3rd Year BSIT</Text>
+      <Text>Mobile App Development</Text>
+    </View>
+  );
+}
